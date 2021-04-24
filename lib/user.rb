b13c8e4394340edd5e649a7719b497d92bb27c57
Cody Alvarez <cody_alvarez19@outlook.com>
2021-04-24 23:26:17 +0000

    Automatically backed up by Learn

--- lib/user.rb
+++ lib/user.rb
@@ -1,11 +1,15 @@
 class User
   attr_accessor :first_name, :last_name
 
+  KNOWLEGDE = []
+
   def initialize
     @first_name = first_name
     @last_name = last_name
+
   end
 
   def new
+
   end
 end
